ddfb4c6164113aa99e10c4db3c9ff330b9ba0e61
Zarif Rani <zariffathurrahman59@gmail.com>
2024-03-30 13:52:13 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+## we-toolkit-telegram
+Get info from WE Telecom Egypt api and sent it via Telegram
+
+## Requirement
+Node v18.20.0
+
+v20.12.0 is not recommend
+
+## Installation
+
+`git clone`
+
+`npm install`
+
+## Usage
+
+1. Change WE_LOGIN_NUMBER and WE_LOGIN_PASSWORD in run.js
+2. Get telegram bot api token from telegram @BotFather
+3. Get chat id by using `https://api.telegram.org/bot<YourBOTToken>/getUpdates`
+4. Run the script `node run.js`
+
+## Example response
+
+![response](response.png)
+
+## Acknowledgements
+
+- we-toolkit from [kofta999](https://github.com/kofta999)
---
+++ run.js
@@ -0,0 +1,32 @@
+import { getUsage, login } from "we-toolkit";
+import TelegramBot from 'node-telegram-bot-api';
+
+let authData = await login("WE_LOGIN_NUMBER", "WE_LOGIN_PASSWORD"); // => change WE_LOGIN_NUMBER WE_LOGIN_PASSWORD
+let usage = await getUsage(authData);
+
+// getting info from api server
+const weInfo = () => {
+    const keys = Object.keys(usage.detailedLineUsageList);
+    const C_TED_Primary_Fixed_Data = keys[keys.length -1];
+    const info = `Name: ${usage.summarizedLineUsageList[0].englishDisplayName} \nTotal Amount: ${usage.summarizedLineUsageList[0].initialTotalAmount}GB \nBalance: ${usage.summarizedLineUsageList[0].freeAmount}GB \nUsed: ${usage.summarizedLineUsageList[0].usedAmount}GB \nUsage Percentage: ${usage.summarizedLineUsageList[0].usagePercentage}% \nRenewal Date: ${usage.detailedLineUsageList[C_TED_Primary_Fixed_Data].renewalDate}`;
+    return info
+}
+
+// send it via telegram
+const sendTelegram = () => {
+    const token = 'YOUR_TELEGRAM_BOT_TOKEN'; // => change YOUR_TELEGRAM_BOT_TOKEN 
+    const bot = new TelegramBot(token, {polling: true});
+
+    bot.sendMessage(CHAT_ID, weInfo()) // => change CHAT_ID 
+    .then(() => {
+        console.log('done sending');
+        process.exit(0);
+    })
+    .catch((error) => {
+        console.log(error.code);  // => 'ETELEGRAM'
+        console.log(error.response.body); // => { ok: false, error_code: 400, description: 'Bad Request: chat not found' }
+        process.exit(0);
+    });
+}
+
+sendTelegram()
